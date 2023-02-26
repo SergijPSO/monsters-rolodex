3d6246cd9155db14ae3b024760d7e133c7c34c38
Sergij Pankiv <pankivsergij97@gmail.com>
2023-02-26 10:15:08 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {Component} from 'react';
-
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,31 +7,49 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Sergiy',
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
-  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') 
+    .then((response) =>  response.json())
+    .then((users) => this.setState(
+      () => {
+      return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+    ));
+  }
+
   render() {
+    console.log('render');
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
+        <input className='serach-box' type='search' placeholder='Search for monster' onChange={(event)=> {
+          console.log(event.target.value)
+          const searchField = event.target.value.toLowerCase()
+         
+          this.setState(()=> {
+            return {searchField};
+          })
 
-            <button
-              onClick = { ()=> {
-                this.setState({name: 'Another name'})
-              }}
-            >
-              
-            Change name
-          </button>
-        </header>
+        }}/>
+        {filteredMonsters.map((monster)=> {
+          return <div key={monster.id}>
+            <h1> {monster.name} </h1>
+          </div>
+        })}
       </div>
-    );
+    )
+
   }
 }
 
