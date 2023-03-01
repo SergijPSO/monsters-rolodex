cbf53a31de5ef348a21a2b79e6faaca6d3a5453b
Sergij Pankiv <pankivsergij97@gmail.com>
2023-03-01 19:54:46 +0200

    Styles updates was made

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
   }
 
   render() {
-
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
@@ -42,6 +41,8 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        
         <SearchBox
           className={'search-box'}
           onChangeHandler={onSearchChange}
